477a1a73d8adc248ccbd2ac1c798a2402018d664
Jose Toro <jtoro2716@gmail.com>
2014-10-04 09:27:06 -0500

    Liquify

---
+++ liquid.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+PRODUCT_NAME := liquid_d2lte
